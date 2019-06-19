2f022e86c5b1df50247e92a46fae7c42063cc424
zhong_zhangzhi <zhongzhangzhi@xiniaoyun.com>
2019-06-19 16:54:37 +0800

    initial

---
+++ mine/activemq-cluster/src/main/java/com/study/mq/activemq/ActivemqClusterApplication.java
@@ -0,0 +1,13 @@
+package com.study.mq.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqClusterApplication.class, args);
+    }
+
+}
